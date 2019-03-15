3b9192a632c23874afe471183174389b20d14e6d
Anastas <misailidi.a@gmail.com>
2019-03-15 20:48:09 +0300

    Edited functions
    -init random weights and biases
    -input weights and biases into the file
    Now works correctly

--- neural _network/DigitRecognition/DigitRecognition/functions.py
+++ neural _network/DigitRecognition/DigitRecognition/functions.py
@@ -30,27 +30,37 @@ def initWB(layers_dims):
     np.random.seed(1)
     parameters = {}
     L = len(layers_dims)
-
     for l in range(1, L):
         parameters["W" + str(l)] = np.random.randn(
             layers_dims[l], layers_dims[l-1]) * 0.01
-        parametrs["b" + str(l)] = np.zeros((layers_dims[l], 1))
+        parameters["b" + str(l)] = np.zeros((layers_dims[l], 1))
     return parameters
 
 #Функция записи параметров в файл
 def inputWB(parameters):
-    count = 0
-    while parameters.get("W" + str(count)) != None:
-        file = open('W' + str(count) + '.txt', 'a')
+    count = 1
+    string = ''
+    while np.all(parameters.get("W" + str(count))) != None:
+        file = open('W' + str(count) + '.txt', 'w')
         for i in parameters["W" + str(count)]:
-            file.write(' '.join(i))
+            for j in i:
+                string += str(j) + ' '
+            file.write(string + '\n')
+            string = ''
         file.close()
         count += 1
-    count = 0
-    while parameters.get("b" + str(count)) != None:
-        file = open('b' + str(count) + '.txt', 'a')
+    count = 1
+    string = ''
+    while np.all(parameters.get("b" + str(count))) != None:
+        file = open('b' + str(count) + '.txt', 'w')
         for i in parameters["b" + str(count)]:
-            file.write(' '.join(i))
+            for j in i:
+                string += str(j) + ' '
+            file.write(string + '\n')
+            string = ''
         file.close()
         count += 1
     return 0
+
+#print(initWB([10, 4, 2]))
+print(inputWB(initWB([10, 4, 2])))
