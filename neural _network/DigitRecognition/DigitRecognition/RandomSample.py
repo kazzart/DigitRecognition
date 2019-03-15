0b4c39f414955a38a53a4743408024622770cc8a
Artyom <temchik00@yandex.ru>
2019-03-15 21:34:12 +0300

    Random jpg load+nums
    Added function for loading random jpg from /samples/learning and added few numbers

---
+++ neural _network/DigitRecognition/DigitRecognition/RandomSample.py
@@ -0,0 +1,41 @@
+import random as r
+from PIL import Image
+class Rand:
+    used = []
+    #инициализация, quantity - количество примеров
+    def __init__(self,quantity):
+        r.seed(version =2)
+        for i in range(10):
+            li = []
+            for i in range(quantity):
+                li.append(i+1)
+            self.used.append(li)
+    #проверка, не осталось ли неиспользованных примеров
+    def notEmpty(self):
+        for line in self.used:
+            if len(line) > 0:
+                return True
+        return False
+    def show(self):
+        for i in self.used:
+            print(i)
+    #выдаёт следующее рандомное изображение
+    def next(self):
+        li = self.used
+        a = r.randint(0,len(li)-1)
+        while len(li[a]) == 0:
+            a = r.randint(0,len(li)-1)
+        print(str(a) + " , " + str(li[a]))
+        li2 = li[a]
+        im = Image.open("..\..\..\samples\learn\{}\test_{}_{}.jpg".format(a, a, li2[0]))
+        li2.remove(li2[0])
+        li[a] = li2
+        self.used = li
+        return im
+        
+
+'''
+s = Rand(30)
+while(s.notEmpty()):
+    image = s.next()
+'''
