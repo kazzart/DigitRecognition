d5896606ee53dd493af70b11f204f7f448bb562c
Anastas <misailidi.a@gmail.com>
2019-03-17 14:14:00 +0300

    Some code

--- neural _network/DigitRecognition/DigitRecognition/program.py
+++ neural _network/DigitRecognition/DigitRecognition/program.py
@@ -14,7 +14,8 @@ def L_layer_model(
 
     for i in range(num_iterations):
         AL, caches = fn.L_model_forward(X, parameters)
-        cost = fn.compute_cost(AL,y)
+        #print (AL)
+        cost = fn.compute_cost(AL, y)
         grads = fn.L_model_backward(AL, y, caches)
         parameters = fn.update_parameters(parameters, grads, learning_rate)
         if (i + 1) % 100 == 0 and print_cost:
@@ -40,8 +41,8 @@ s = Rand(60)
 if s.notEmpty():
     image, num = s.next()
     X1, y1 = fn.take_a_pic(image, num)
-    X = np.array([X1]).T
-    y = np.array([y1]).T
+    X_train = np.array([X1]).T
+    y_train = np.array([y1]).T
     #print (np.shape(X), np.shape(y))
 
 while(s.notEmpty()):
@@ -49,17 +50,35 @@ while(s.notEmpty()):
     X1, y1 = fn.take_a_pic(image, num)
     X2 = np.array([X1]).T
     y2 = np.array([y1]).T
-    X_train = np.concatenate((X, X2), axis = 1)
-    y_train = np.concatenate((y, y2), axis = 1)
-print (np.shape(X_train), np.shape(y_train))
+    X_train = np.concatenate((X_train, X2), axis = 1)
+    y_train = np.concatenate((y_train, y2), axis = 1)
+    print (np.shape(X_train), np.shape(y_train))
 
+t = Rand(10, 'test')
+if t.notEmpty():
+    image, num = s.next()
+    X1, y1 = fn.take_a_pic(image, num)
+    X_test = np.array([X1]).T
+    y_test = np.array([y1]).T
+    #print (np.shape(X), np.shape(y))
+
+while(s.notEmpty()):
+    image, num = s.next()
+    X1, y1 = fn.take_a_pic(image, num)
+    X2 = np.array([X1]).T
+    y2 = np.array([y1]).T
+    X_test = np.concatenate((X_test, X2), axis = 1)
+    y_test = np.concatenate((y_test, y2), axis = 1)
+    print (np.shape(X_test), np.shape(y_test))
+
+#print (X_train/255)
 
 layers_dims = [X_train.shape[0], 30, 10]
 parameters = L_layer_model(
-    X_train, y_train, layers_dims,
+    X_train/255, y_train, layers_dims,
     learning_rate = 0.03, num_iterations = 3000)
 fn.input_parameters(parameters)
 
-accuracy(X_train, parameters, y_train)
+print (accuracy(X_test/255, parameters, y_test))
 
     
